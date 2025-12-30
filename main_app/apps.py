2ecf8a65fb7e1da9e3bb5114e3d1008511b34376
A SHUBHAM PATRO <shubhampatro107@gmail.com>
2025-12-30 22:31:36 +0530

    Register MainAppConfig

--- main_app/apps.py
+++ main_app/apps.py
@@ -1,5 +1,18 @@
 from django.apps import AppConfig
 
-
 class MainAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'main_app'
+
+    def ready(self):
+        try:
+            from .models import CustomUser
+
+            if not CustomUser.objects.filter(email="admin@gmail.com").exists():
+                CustomUser.objects.create_superuser(
+                    email="admin@gmail.com",
+                    password="admin123",
+                    user_type=1
+                )
+        except Exception:
+            pass
